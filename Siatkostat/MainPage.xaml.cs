bd2527d7354118da8cc240064b3c99e3d102dade
Mamelski <jakub.mamelski@gmail.com>
2014-11-13 13:17:19 +0100

    Nawigacja wstecz dodana do jednej strony póki co

--- Siatkostat/MainPage.xaml.cs
+++ Siatkostat/MainPage.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
@@ -25,8 +14,6 @@ namespace Siatkostat
         public MainPage()
         {
             InitializeComponent();
-           
-
             NavigationCacheMode = NavigationCacheMode.Required;
         }
 
@@ -44,6 +31,7 @@ namespace Siatkostat
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+
         }
 
         private void MatchStartButton_Click(object sender, RoutedEventArgs e)
