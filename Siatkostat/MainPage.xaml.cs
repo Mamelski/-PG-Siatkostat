4f08e3793a52386a3d2420d15c1ceb113a576a50
Mamelski <jakub.mamelski@gmail.com>
2014-11-17 11:49:56 +0100

    Main Page refaktor. Button edit team jest ukryty jeśli user jest wylogowany

--- Siatkostat/MainPage.xaml.cs
+++ Siatkostat/MainPage.xaml.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using Windows.Phone.UI.Input;
-using Windows.UI.Popups;
+﻿using Windows.Phone.UI.Input;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
-using Microsoft.WindowsAzure.MobileServices;
 
 namespace Siatkostat
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
@@ -23,22 +17,13 @@ namespace Siatkostat
             NavigationCacheMode = NavigationCacheMode.Required;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             HardwareButtons.BackPressed += HardwareButtons_BackPressed;
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-           
+            if (App.SelectedTeam == null)
+            {
+               EditTeamButton.Visibility = Visibility.Collapsed;
+            }
         }
       
 
@@ -57,6 +42,12 @@ namespace Siatkostat
             e.Handled = true;
             HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
             Application.Current.Exit();
+        }
+
+        private void EditTeamButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(EditTeam));
+        
         }
     }
 }
